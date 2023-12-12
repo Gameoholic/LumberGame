df95ff9957991acb3cb726c25cd117085b02852a
Game <viner.atmic@gmail.com>
2023-12-12 20:46:10 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    id ("java")
+
+    id("io.papermc.paperweight.userdev") version "1.5.5"
+    id("xyz.jpenilla.run-paper") version "2.1.0" // Adds runServer and runMojangMappedServer tasks for testing
+
+    //Shading & relocating into the plugin JAR
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "xyz.gameoholic"
+version = "1.0"
+description = "Lumber game."
+val apiVersion = "1.20"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.dmulloy2.net/repository/public/")
+    }
+    maven {
+        url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    }
+    maven {
+        url = uri("https://repo.gameoholic.xyz/releases")
+    }
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT") //the paper dev bundle is a compile-only dependency, paper itself provides it. No need to shade
+
+    compileOnly ("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+}
+
+java {
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+//java {
+//    def javaVersion = JavaVersion.toVersion(targetJavaVersion)
+//    sourceCompatibility = javaVersion
+//    targetCompatibility = javaVersion
+//    if (JavaVersion.current() < javaVersion) {
+//        toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+//    }
+//}
+
+
+
+tasks {
+    // Configure reobfJar to run when invoking the build task
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+        // See https://openjdk.java.net/jeps/247 for more information.
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+        val props = mapOf(
+            "name" to project.name,
+            "version" to project.version,
+            "description" to project.description,
+            "apiVersion" to apiVersion
+        )
+        inputs.properties(props)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+
+
+    shadowJar {
+        // helper function to relocate a package into our package
+        fun reloc(pkg: String) = relocate(pkg, "${project.group}.${project.name}.dependency.$pkg")
+
+        //relocate("kotlin", "xyz.gameoholic.partigon.dependency.kotlin")
+    }
+
+}
+
---
+++ src/main/java/xyz/gameoholic/lumbergame/LumberGame.java
@@ -0,0 +1,17 @@
+package xyz.gameoholic.lumbergame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LumberGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
