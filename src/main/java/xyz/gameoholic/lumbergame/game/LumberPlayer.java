2d157f528e4ecbfa7a8f3ebda4a785482839b170
Game <viner.atmic@gmail.com>
2023-12-15 21:02:43 +0200

    feat: add currencies to player

--- src/main/java/xyz/gameoholic/lumbergame/game/LumberPlayer.java
+++ src/main/java/xyz/gameoholic/lumbergame/game/LumberPlayer.java
@@ -4,6 +4,10 @@ import java.util.UUID;
 
 public class LumberPlayer {
     private UUID uuid;
+    private int wood = 0;
+    private int iron = 0;
+    private int gold = 0;
+    private int diamond = 0;
 
     public LumberPlayer(UUID uuid) {
         this.uuid = uuid;
